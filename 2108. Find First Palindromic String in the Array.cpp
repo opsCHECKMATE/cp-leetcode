2c37d79dcf0bfc5255429e1161bb7820a3b8e7b2
Mahmoud H Hamdy <lazy.coder.dev.exe@gmail.com>
2026-06-10 17:40:45 +0300

    Create 2108. Find First Palindromic String in the Array.cpp

---
+++ 2108. Find First Palindromic String in the Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool is_ok(const string &s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l++] != s[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    string firstPalindrome(const vector<string>& x) {
+        for (int i = 0; i < (int)x.size(); ++i) {
+            if (is_ok(x[i])) {
+                return x[i];
+            }
+        }
+        return "";
+    }
+};
