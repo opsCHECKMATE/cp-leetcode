ab4bc909e628db96d90f0c258dd42502ba8c3d54
Mahmoud H Hamdy <lazy.coder.dev.exe@gmail.com>
2026-06-10 17:14:31 +0300

    Update 217. Contains Duplicate.cpp

--- 217. Contains Duplicate.cpp
+++ 217. Contains Duplicate.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    bool containsDuplicate(const vector<int>& x) {
-        unordered_map<int, int> mp;
-        for(auto i: x) {
-            mp[i]++;
-        }
-        for(auto i: mp) {
-            if(i.second > 1) {
+    bool containsDuplicate(const vector<int>& a) {
+        unordered_set<int> s;
+        for (int x : a) {
+            if (!s.insert(x).second) {
                 return true;
             }
         }
